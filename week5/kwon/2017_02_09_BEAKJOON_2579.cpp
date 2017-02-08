986af4633663c37f3557b3ad7092441518221c06
kwoncesc <kwoncesc@gmail.com>
2017-02-09 01:00:35 +0900

    :)

--- week5/kwon/2017_02_09_BEAKJOON_2579.cpp
+++ week5/kwon/2017_02_09_BEAKJOON_2579.cpp
@@ -4,20 +4,21 @@
 using namespace std;
 
 int T;
-long long stair[301]; // 각 계단의 값 저장
-long long temp[301]; // 계단을 오르면서 최대값을 저장
+long long stair[301]; 
+long long temp[301]; // 동적계산위해 
 
 void dynamic(void) 
 {
 	for (int i = 0; i <= T; i++) 
 	{
-		if (i == 0) // 시작점 값은 0
+		// 패턴에서 착안   
+		if (i == 0)
 			temp[i] = 0;
-		else if (i == 1) // 첫번째 계단
+		else if (i == 1) 
 			temp[i] = stair[i];
-		else if (i == 2) // 두번째 계단
+		else if (i == 2) 
 			temp[i] = max(stair[i - 1] + stair[i], temp[i - 2] + stair[i]);
-		else // 3번째 계단
+		else 
 			temp[i] = max(temp[i - 3] + stair[i - 1] + stair[i], temp[i - 2] + stair[i]);
 	}
 }
