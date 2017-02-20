dac708e6a1592f1810df8ec59ae5a786662faca8
yubi.lee <yubi.lee@AL01205764.local>
2017-02-21 08:14:50 +0900

    not finish yet

--- week6/eub/2618_copcar.cpp
+++ week6/eub/2618_copcar.cpp
@@ -6,13 +6,19 @@ using namespace std;
 
 int n, w;
 int incident[1000+5][2+5];
+int alt = 0;
 // [cop1위치][cop2위치][사건인덱스]
 // '위치'가 나타내는 숫자는 어떤 사건을 가리킨다. 즉, 그 사건이 일어난 위치에 있다는 뜻
 // cache에서의 0 인덱스는 시작점을 나타내고, incident에서는 [0] [w+1]이 각각 좌표를 나타낸다.
-int cache[1000+5][1000+5][1000+5];
+// 3번째 인덱스가 0이라면 바로 이전 layer를 1이라면 현재 레이어를 말함?
+// alternative?
+int cache[1000+5][1000+5][2];
 
 // 어디서에서 이 자리에 오게 되었는지
-int parent[1000+5][1000+5][1000+5][2];
+
+//TODO: 이렇게 많은 배열이 필요 없을 수 있다. 사건을 순서대로 처리한다면.. 배열이 꽉 차지 않는다.ㄴ
+// 자식은 여럿이지만 부모는 하나..? 가장 짧은 길을 택하니까
+int parent[1000+5][1000+5][2];
 
 int distance(int* a, int* b) {
     int diff = abs(a[0]-b[0]) + abs(a[1]-b[1]);
@@ -21,31 +27,31 @@ int distance(int* a, int* b) {
 
 void dp(int numOfIncidents) {
     // base case
-    cache[1][0][1] = distance(incident[1], incident[0]);
-    cache[0][1][1] = distance(incident[1], incident[w+1]);
+    cache[1][0][alt] = distance(incident[1], incident[0]);
+    cache[0][1][alt] = distance(incident[1], incident[w+1]);
 
-    parent[1][0][0][0] = parent[1][0][0][1] =  1;
-    parent[0][1][1][0] = parent[0][1][1][1] =  n;
+    parent[1][0][alt] = parent[1][0][alt] =  1;
+    parent[0][1][alt] = parent[0][1][alt] =  n;
 
     for(int i = 2; i <= numOfIncidents; i++) {
         // cop1이 현재 사건으로 가는 경우
         for(int j = 0; j < i; j++) {
-            cache[i][j][i] = 987654321; // 초기화
+            cache[i][j][alt] = 987654321; // 초기화
         }
         // 바로 전 사건도 cop1이 처리한 경우
         for(int j = 0; j < i-1; j++) {
-            cache[i][j][i] = cache[i-1][j][i-1] + distance(incident[i-1], incident[i]);
+            cache[i][j][alt] = cache[i-1][j][alt ^ 1] + distance(incident[i-1], incident[i]);
             // 바로 전에 어느 위치에서 왔나.
-            parent[i][j][i][0] = i-1;
-            parent[i][j][i][1] = j;
+            parent[i][j][0] = i-1;
+            parent[i][j][1] = j;
         }
 
         // 바로 전 사건은 cop2가 처리한 경우
         for(int j = 0; j < i-1; j++) {
-            if (cache[i][i-1][i] > cache[j][i-1][i-1] + distance(incident[j], incident[i]) ) {
-                cache[i][i-1][i] = cache[j][i-1][i-1] + distance(incident[j], incident[i]);
-                parent[i][i-1][i][0] = j;
-                parent[i][i-1][i][1] = i-1;
+            if (cache[i][i-1][alt] > cache[j][i-1][alt ^ 1] + distance(incident[j], incident[i]) ) {
+                cache[i][i-1][alt] = cache[j][i-1][alt ^ 1] + distance(incident[j], incident[i]);
+                parent[i][i-1][0] = j;
+                parent[i][i-1][1] = i-1;
             }
         }
 
@@ -55,20 +61,21 @@ void dp(int numOfIncidents) {
         }
         // 바로 전 사건도 cop2이 처리한 경우
         for(int j = 0; j < i-1; j++) {
-            cache[j][i][i] = cache[j][i-1][i-1] + distance(incident[i-1], incident[i]);
+            cache[j][i][alt] = cache[j][i-1][alt ^ 1] + distance(incident[i-1], incident[i]);
             // 바로 전에 어느 위치에서 왔나.
-            parent[j][i][i][0] = j;
-            parent[j][i][i][1] = i-1;
+            parent[j][i][0] = j;
+            parent[j][i][1] = i-1;
         }
 
         // 바로 전 사건은 cop1가 처리한 경우
         for(int j = 0; j < i-1; j++) {
-            if (cache[i-1][i][i] > cache[i-1][j][i-1] + distance(incident[j], incident[i])) {
-                cache[i-1][i][i] = cache[i-1][j][i-1] + distance(incident[j], incident[i]);
-                parent[i-1][i][i][0] = i-1;
-                parent[i-1][i][i][1] = j;
+            if (cache[i-1][i][alt] > cache[i-1][j][alt ^ 1] + distance(incident[j], incident[i])) {
+                cache[i-1][i][alt] = cache[i-1][j][alt ^ 1] + distance(incident[j], incident[i]);
+                parent[i-1][i][0] = i-1;
+                parent[i-1][i][1] = j;
             }
         }
+        alt ^= 1;
     }
 }
 
@@ -84,8 +91,8 @@ void printOrder(int endCop1, int endCop2) {
       } else {
         order.push_back(2);
       }
-      endCop1 = parent[endCop1][endCop2][i][0];
-      endCop2 = parent[endCop1][endCop2][i][1];
+      endCop1 = parent[endCop1][endCop2][0];
+      endCop2 = parent[endCop1][endCop2][1];
   }
   for(int i = w; i >= 1; i--) {
     printf("%d\n", order.back());
@@ -112,23 +119,25 @@ int main(void) {
     dp(w);
 
     for(int i = 0; i < w; i++) {
-        int tmpEndCop1, tmpEndCop2;
-        int min;
-        if(cache[i][w][w] < cache[w][i][w]) {
-          min = cache[i][w][w];
-          tmpEndCop1 = i;
-          tmpEndCop2 = w;
-        } else {
-          min = cache[w][i][w];
-          tmpEndCop1 = w;
-          tmpEndCop2 = i;
-        }
-        //int min = cache[i][w][w] < cache[w][i][w] ? cache[i][w][w] : cache[w][i][w];
-        //curMin = curMin < min ? curMin : min;
-        if( min < curMin) {
-          curMin = min;
-          endCop1 = tmpEndCop1;
-          endCop2 = tmpEndCop2;
+        for(int j = 0; j < 2; j++) {
+            int tmpEndCop1, tmpEndCop2;
+            int min;
+            if(cache[i][w][j] < cache[w][i][j]) {
+              min = cache[i][w][j];
+              tmpEndCop1 = i;
+              tmpEndCop2 = w;
+            } else {
+              min = cache[w][i][j];
+              tmpEndCop1 = w;
+              tmpEndCop2 = i;
+            }
+            //int min = cache[i][w][w] < cache[w][i][w] ? cache[i][w][w] : cache[w][i][w];
+            //curMin = curMin < min ? curMin : min;
+            if( min < curMin) {
+              curMin = min;
+              endCop1 = tmpEndCop1;
+              endCop2 = tmpEndCop2;
+            }
         }
     }
     printf("%d\n",curMin);
