1034a5137aea01800eabe7f588e511e20c455594
yubi.lee <yubi.lee@AL01205764.local>
2017-02-28 08:13:36 +0900

    내가 짠 코드와 비교가 필요하다.
     - 교재에 있는 코드를 참고하여 답안을 제출했을 때는 통과하였으나, 바로 이전 커밋인 내가 짠 코드는 답이 틀렸다. 원인을 찾아보자.

--- week7/eub/zimbabwe.cpp
+++ week7/eub/zimbabwe.cpp
@@ -7,54 +7,52 @@ using namespace std;
 
 int c;
 int m;
-long long eVal;
-char e[15];
-int sortedE[15];
 int len;
-
-// 초기화 필요
-int taken;
-
-///////////////////////
-//int cache[1<<16][10][2];
-int cache[1<<16][20][2];
 
 int cmpFunc (const void *a, const void *b) {
   return (*(int*)a - *(int*)b);
 }
 
+const int MOD = 1000000007;
+string e;
+string digit;
+int n;
+int cache[1<<14][20][2];
 
-int solve(int curIdx, int mod, bool pass, int taken) {
-  if(curIdx == len) {
-    ////////////
-    return pass && mod == 0;
-  }
-  int& ret = cache[taken][mod][pass];
-  if(ret != -1)
-    return ret;
-/////////////////
-  ret = 0;
-  // int limit = 9;
-  // if(pass == false) {
-  //   limit = e[curIdx];
-  // }
-  for(int i=0; i<len; i++) {
-    /*
-      1 == true (true)
-      2 == true (false)
-    */
-    if((taken & (1 << i)) > 0) {
-      continue;
-    }
-    if(!pass && e[curIdx] < sortedE[i]) {
-      continue;
-    }
-    if(0 < i && sortedE[i] == sortedE[i-1] && ((taken & (1 << (i-1)) ) == false) ) {
-      continue;
-    }
-    ret += (solve(curIdx+1, (mod * 10 + sortedE[i]) % m, pass || (sortedE[i] < e[curIdx]), taken | (1 << i))) % 1000000007;
-  }
-  return ret;
+
+int price(int index, int taken, int mod, int less)
+{
+	if (index == n)
+	{
+		return (less && mod == 0) ? 1 : 0;
+	}
+
+	int& ret = cache[taken][mod][less];
+	if (ret != -1)
+	{
+		return ret;
+	}
+	ret = 0;
+
+	for (int next = 0; next < n; next++)
+	{
+		if ((taken & (1 << next)) == 0)
+		{
+			if (!less && e[index] < digit[next])
+				continue;
+
+			if (next > 0 && digit[next - 1] == digit[next] && (taken & (1 << (next - 1))) == 0)
+				continue;
+
+			int nextTaken = taken | (1 << next);
+			int nextMod = (mod * 10 + digit[next] - '0') % m;
+			int nextLess = less || e[index] > digit[next];
+			ret += price(index + 1, nextTaken, nextMod, nextLess);
+			ret %= MOD;
+
+		}
+	}
+	return ret;
 }
 
 
@@ -63,18 +61,24 @@ int main(void) {
   scanf("%d", &c);
 
   while(c--) {
-    memset(cache, -1, sizeof(int)*(1<<16)*20*2);
-    taken = 0;
+    memset(cache, -1, sizeof(int)*(1<<14)*20*2);
 
-    scanf("%lld", &eVal);
-    scanf("%d", &m);
-    len = sprintf(e, "%lld", eVal);
-    for(int i=0; i<len; i++) {
-      sortedE[i] = e[i] -= '0';
+    cin >> e >> m;
+    n = e.length();
+    digit = e;
+
+    // 순서대로 문자 정렬하기
+    // 문자가 많아봐야 14개니까 버블정렬 사용
+    for(int i = 0; i < n - 1; i++) {
+      for(int j = 0; j < n - 1 - i; j++) {
+        if(digit[j+1] < digit[j]) {
+          char tmp = digit[j];
+          digit[j] = digit[j+1];
+          digit[j+1] = tmp;
+        }
+      }
     }
-    qsort(sortedE, len, sizeof(int), cmpFunc);
-    printf("%d\n", solve(0,0,0,0));
-
+    cout << price(0, 0, 0, 0) << endl;
 
   }
 
