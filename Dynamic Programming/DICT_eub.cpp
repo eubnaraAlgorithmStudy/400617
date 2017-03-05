1bde779c74c705a7eecc26e15c822b56585b4384
yubi Lee <eubnara@gmail.com>
2017-03-05 11:53:33 +0900

    update comment

--- Dynamic Programming/DICT_eub.cpp
+++ Dynamic Programming/DICT_eub.cpp
@@ -31,6 +31,11 @@ int prepCal(int a, int b) {
     ret += prepCal(a - 1, b);
     ret += prepCal(a, b - 1);
   }
+  //error: ret %= K_MAX; 로 하면 답이 틀리다.
+  //왜냐하면, 최대값이 채워지는 게 아니라, 나머지가 채워지므로 K_MAX보다 작은 값이 들어갈 수도 있고,
+  //이는 원하는 동작이 아니다.
+  //printKthWord()에서 prepCal(n, m)을 호출하였을 때, 숫자가 많아서 K_MAX보다 당연히 훨씬 많은데
+  //잘못된 계산으로 NONE이 출력되었었다.
   if(ret > K_MAX)
     ret = K_MAX;
 
@@ -44,7 +49,6 @@ void printKthWord(int k) {
   }
   int a = n;
   int b = m;
-  //printf("a, b = %d %d\n", a, b);
   while (a != 0 && b != 0) {
     if (cache[a - 1][b] >= k) {
       printf("a");
