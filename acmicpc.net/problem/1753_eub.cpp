3209dc5cc120bba0128179a7bfe639a295308161
yubi Lee <eubnara@naver.com>
2018-02-05 01:38:27 +0900

    [acmicpc/problem#1753]
    - 우선순위 큐에서 이미 들어가 있는 값을 변경한 후에 다시 힙 형태로 만들려고 하니, 시간초과.
    - 그냥 값만 바꾸면 힙 상태가 다시 자연스레 갱신되지 않는다.
    - 차라리, 추가로 <vertex, weight> 를 우선순위 큐에 넣으면, min_heap 으로 구성했으니, 가장 작은 것이 먼저 튀어나올 것이다.
    - isCompleted 같은 flag 변수를 이용하여 나중에 중복으로 들어간 정점은 자연스레 제거된다.
    - 얼마나 많이 우선순위 큐에 들어갈지 따져보면, 최악의 경우는 `총 정점의 수(V) + 총 간선의 수(E)` 일 것이다.
      - 모든 간선이 시작 정점에서 출발할 때

--- acmicpc.net/problem/1753_eub.cpp
+++ acmicpc.net/problem/1753_eub.cpp
@@ -14,10 +14,10 @@ typedef struct Edge {
 
 typedef struct Vertex {
     int v;
-    int* d;
-    Vertex(int V, int* D) : v(V), d(D) { }
+    int d;
+    Vertex(int V, int D) : v(V), d(D) { }
     bool operator<(const Vertex& a) {
-        return (a.d) < d;
+        return (a.d) > d;
     }
     bool operator=(const Vertex& a) {
         v = a.v;
@@ -26,13 +26,17 @@ typedef struct Vertex {
 
 } VERTEX;
 
-
-
-bool comp(const VERTEX& a, const VERTEX& b) {
-    return (a.d) > (b.d);
+bool operator<(VERTEX a, VERTEX b){
+    return a.d > b.d;
 }
 
 
+// struct comp {
+//   bool operator()(const VERTEX& a,const VERTEX& b) const{
+//     return *a.d < *b.d;
+//   }
+// };
+bool isCompleted[20000+1];
 
 int main(void) {
     int V, E, start;
@@ -40,32 +44,39 @@ int main(void) {
     scanf("%d", &start);
     int dist[20000+1];
     list<EDGE> edge[300000+1];
-    vector<VERTEX> q;
+    priority_queue<VERTEX> q;
     for(int i = 1; i <= V; i++) {
         dist[i] = INF;
-        q.push_back(VERTEX(i, &dist[i]));
+        if (start != i) {
+            q.push(VERTEX(i, dist[i]));
+        }
     }
     
     for(int i = 0; i < E; i++) {
         int from, to, w;
         scanf("%d %d %d", &from, &to, &w);
-        if (start == from && w < dist[to]) {
-            dist[to] = w;
-        } else if (start == to) {
+        if (start == to) {
             continue;
         } else {
             edge[from].push_back(EDGE(to, w));
         }
     }
-
+    q.push(VERTEX(start, 0));
     dist[start] = 0;
     while(!q.empty()) {
-        make_heap(q.begin(), q.end());
-        VERTEX cur = q.front();
-        pop_heap(q.begin(), q.end()); q.pop_back();
+        VERTEX cur = q.top();
+        q.pop();
+        if(isCompleted[cur.v]) {
+            continue;    
+        } else {
+            isCompleted[cur.v] = true;
+        }
         for(auto it = edge[cur.v].begin(); it != edge[cur.v].end(); it++) {
-            if (0 <= *cur.d + it->w && *cur.d + it->w < dist[it->to]) {
-                dist[it->to] = *cur.d + it->w;
+            if (0 <= cur.d + it->w && cur.d + it->w < dist[it->to]) {
+                dist[it->to] = cur.d + it->w;
+                if (!isCompleted[it->to]) {
+                    q.push(VERTEX(it->to, dist[it->to]));
+                }
             }
         }
     }
@@ -74,7 +85,7 @@ int main(void) {
         if(INF == dist[i]) {
             printf("INF\n");
         } else {
-            printf("%d\n", dist[i]);            
+            printf("%d\n", dist[i]);
         }
     }
 
